a18e50e6e09973288a1be9bea3974659f772080f
daniel <daniel@danspc>
2020-03-13 16:34:17 -0400

    New Logo

---
+++ app/src/main/assets/messages.js
@@ -0,0 +1,27 @@
+var startMessage = [
+	["This is the Nothing Game."],
+	["No explanation required. Don't do anything, and you will win the game."],
+	["This is not a joke. You just have to do nothing."],
+	["You cannot listen to music, switch to another tab, or do anything that isn't this game."],
+	["Don't press any buttons. Don't ever press the G key."],
+	["It is the most simple thing you could ever ask a human being to do, to not do anything."],
+	["The Nothing Game starts now."],
+	[""]
+]
+
+var wait5 = [
+	["It's been 5 minutes since you started doing nothing. I’ll check on you in 10 minutes."]
+]
+
+var wait15 = [
+	["15 entire minutes of nothing! See you in another 15 minutes!"]
+]
+
+var wait30 = [
+	["30 minutes of nothing. Good job!"]
+]
+
+var wait60 = [
+	["One hour! You’ve beat the game!"],
+	["It’s finally over. No more waiting now."]
+]
